99c36115947419695c9cd4e0a0a6caed72e3259a
WakyzoneP <vlad_beraru@yahoo.com>
2023-04-16 20:45:09 +0300

    Added Reteta class

---
+++ CabinetMedical/Reteta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabinetMedical
+{
+    internal class Reteta
+    {
+        private readonly string medic;
+        private readonly string pacient;
+        private readonly string diagnostic;
+        private readonly string tratament;
+        private readonly string data;
+        public Reteta(string medic, string pacient, string diagnostic, string tratament, string data)
+        {
+            this.medic = medic;
+            this.pacient = pacient;
+            this.diagnostic = diagnostic;
+            this.tratament = tratament;
+            this.data = data;
+        }
+        public string Medic { get => medic; }
+        public string Pacient { get => pacient; }
+        public string Diagnostic { get => diagnostic; }
+        public string Tratament { get => tratament; }
+        public string Data { get => data; }
+        public override string ToString()
+        {
+            return "Medic: " + medic + " Pacient: " + pacient + " Diagnostic: " + diagnostic + " Tratament: " + tratament + " Data: " + data;
+        }
+    }
+}
